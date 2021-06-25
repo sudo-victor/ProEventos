ca57ca15c3d3943be1a0b77eaf3d3c69deb3ef33
sudo-victor <victor.souto@hotmail.com>
2021-06-25 08:26:00 -0300

    introdução ao EF Core

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,42 +13,22 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
-
+            _context = context;
         }
-
-        public IEnumerable<Evento> _evento = new Evento[] {
-                new Evento() {
-                EventoId = 1,
-                Tema = "Angular 12 e .NET 5",
-                Local = "Belo Horizonte",
-                Lote = "1 Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImageURL = "foto.png"
-                },
-                new Evento() {
-                EventoId = 2,
-                Tema = "Angular 12 e Suas novidades",
-                Local = "São Paulo",
-                Lote = "2 Lote",
-                QtdPessoas = 350,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImageURL = "foto-1.png"
-                }
-        };
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
         [HttpPost]
